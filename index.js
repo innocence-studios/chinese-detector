18ab1d20b5ddaf8f6555bae5521bde8e5d487611
Innocence Studios <153023970+innocence-studios@users.noreply.github.com>
2024-04-06 01:52:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-/*
-let audio = document.createElement('audio');
-audio.src = 'https://audio.jukehost.co.uk/jPp36llbKw9eu0CNQrVhRNq0ManDPRZT';
-document.body.appendChild(audio);
-
-const content = document.body.innerHTML;
-const checked = [];
-let isChinese = false;
-for (let char of content) {
-  if (checked.includes(char) || char.trim() == '') continue;
-  if (isInRange(char.charCodeAt(0).toString(16).toUpperCase(), 0x4E00, 0x9FFF)) isChinese = true;
-  checked.push(char);
-};
-
-let played = false;
-document.addEventListener('click', async () => {
-  if (played || !isChinese) return;
-  played = true;
-  await audio.play();
-});
-
-function isInRange(value, lower, upper) {
-  value = parseInt(value, 16);
-  return value >= lower && value <= upper;
-};
-*/
 
 const audio = new Audio('https://audio.jukehost.co.uk/dMHGd05PDPFPKHXpXvYmz8OJponnhlS4');
 document.body.appendChild(audio);
@@ -47,4 +21,3 @@ setInterval(() => {
     .then(console.log('我在北京 😎'))
     .catch(() => { played = false });
 });
-
